a1384731b590c5f4fe66b8d4ac08fb1703b91204
andrew-redm <andrew.red1993@gmail.com>
2024-09-25 14:42:17 +0100

    - DB function additions and cleaning - naiveElo.py a bad first implementation based on match results

--- Tennis/accessDB.py
+++ Tennis/accessDB.py
@@ -4,7 +4,6 @@ import pandas as pd
 from datetime import datetime, timedelta
 import csv
 
-
 db_path = os.environ.get('ONCOURT_DB_PATH')
 db_password = os.environ.get('ONCOURT_DB_PASSWORD')
 
@@ -20,7 +19,37 @@ def load_replacements(file_path):
 
 replacements = load_replacements('replacements.csv')
 
+
+def player_id_to_name(player_id):
+    query = f'''
+    SELECT NAME_P
+    FROM players_wta
+    WHERE ID_P = {player_id}
+    '''
+    df = pd.read_sql(query, conn)
+    return df['NAME_P'][0] if not df.empty else None
+
+def tournament_id_to_name(tournament_id):
+    query = f'''
+    SELECT NAME_T
+    FROM tours_wta
+    WHERE ID_T = {tournament_id}
+    '''
+    tourneyName = pd.read_sql(query, conn)
+    return tourneyName['NAME_T'][0] if not tourneyName.empty else None
+
+def change_column_names(df, replacements):
+    df = df.rename(columns=replacements)
+    return df
+
 def get_tournaments_in_daterange(start_date, end_date=None):
+    """
+    Retrieves tournaments within a specified date range from the 'tours_wta' table.
+
+    Args:
+        start_date (datetime): The start date of the range.
+        end_date (datetime, optional): The end date of the range. Defaults to the day before the current date if not provided.
+    """
     if end_date is None:
         end_date = datetime.now() - timedelta(days=1)
     query = f'''
@@ -28,21 +57,29 @@ def get_tournaments_in_daterange(start_date, end_date=None):
     FROM tours_wta
     WHERE DATE_T BETWEEN #{start_date}# AND #{end_date.strftime('%Y-%m-%d')}#
     '''
-    df = pd.read_sql(query, conn)
-    df = change_column_names(df, replacements)
-    return df
+    tournaments = pd.read_sql(query, conn)
+    tournaments = change_column_names(tournaments, replacements)
+    return tournaments
 
-def get_matches_in_tournament(tournament_id):   
+def get_matches_in_tournament(tournament_ids):
+    """
+    Retrieves matches from the 'games_wta' table for the given list of tournament IDs.
+
+    Args:
+        tournament_ids (list of int): List of tournament IDs.
+    """
+    tournament_ids_str = ', '.join(map(str, tournament_ids))
     query = f'''
     SELECT games_wta.*, tours_wta.NAME_T, tours_wta.ID_C_T
     FROM games_wta
     INNER JOIN tours_wta ON games_wta.ID_T_G = tours_wta.ID_T
-    WHERE ID_T_G = {tournament_id}
+    WHERE ID_T_G IN ({tournament_ids_str})
     '''
-    df = pd.read_sql(query, conn)
-    df = change_column_names(df, replacements)
-    return df
-
+    matches = pd.read_sql(query, conn)
+    matches = change_column_names(matches, replacements)
+    matches['winnerName'] = matches['ID Winner_G'].apply(player_id_to_name)
+    matches['loserName'] = matches['ID Loser_G'].apply(player_id_to_name)
+    return matches
 
 def get_match_stats(id1, id2, tournament_id):
     query = '''
@@ -51,13 +88,9 @@ def get_match_stats(id1, id2, tournament_id):
     WHERE ((ID1 = ? AND ID2 = ?) OR (ID1 = ? AND ID2 = ?)) AND ID_T = ?
     '''
     params = (int(id1), int(id2), int(id2), int(id1), int(tournament_id))
-    df = pd.read_sql(query, conn, params=params)
-    df = change_column_names(df, replacements)
-    return df
-
-def change_column_names(df, replacements):
-    df = df.rename(columns=replacements)
-    return df
+    stats = pd.read_sql(query, conn, params=params)
+    stats = change_column_names(stats, replacements)
+    return stats
 
 def get_top_100():
     top100WTA = '''
@@ -77,9 +110,9 @@ def get_player_match_odds(player_name):
     INNER JOIN players_wta ON (odds_wta.ID1_O = players_wta.ID_P OR odds_wta.ID2_O = players_wta.ID_P)
     WHERE players_wta.NAME_P = '{player_name}'
     '''
-    df = pd.read_sql(query, conn)
-    df = change_column_names(df, replacements)
-    return df
+    prices = pd.read_sql(query, conn)
+    prices = change_column_names(prices, replacements)
+    return prices
 
 def get_player_match_result(player_name):
     query = f'''
@@ -88,9 +121,9 @@ def get_player_match_result(player_name):
     INNER JOIN tours_wta ON games_wta.ID_T_G = tours_wta.ID_T
     WHERE ID1_G = (SELECT ID_P FROM players_wta WHERE NAME_P = '{player_name}') OR ID2_G = (SELECT ID_P FROM players_wta WHERE NAME_P = '{player_name}')
     '''
-    df = pd.read_sql(query, conn)
-    df = change_column_names(df, replacements)
-    return df
+    results = pd.read_sql(query, conn)
+    results = change_column_names(results, replacements)
+    return results
 
 if not db_path or not db_password:
     raise ValueError("ONCOURT_DB_PATH or ONCOURT_DB_PASSWORD environment variable not set")
@@ -104,10 +137,11 @@ try:
 except pyodbc.Error as e:
     print("Error connecting to the database:", e)
 
-
-# example usage
-# iga_odds = get_player_match_odds('Iga Swiatek')
-# iga_results = get_player_match_result('Iga Swiatek')
-# iga_combined = pd.merge(iga_results, iga_odds, right_on=['P1_ID', 'P2_ID', 'TOUR'], left_on=['ID Winner_G', 'ID Loser_G', 'Tour ID_G'], how='outer')
-# this needs work
-# query = "SELECT * FROM odds_wta WHERE ID1_O = (SELECT ID_P FROM players_wta WHERE NAME_P = 'Jelena Ostapenko') OR ID2_O = (SELECT ID_P FROM players_wta WHERE NAME_P = 'Jelena Ostapenko')"
+def get_todays_matches():
+    query = '''
+    SELECT *
+    FROM today_wta
+    WHERE today_wta.DATE_GAME >= NOW()
+    '''
+    today = pd.read_sql(query, conn)
+    return today
---
+++ Tennis/naiveElo.py
@@ -0,0 +1,75 @@
+from pydantic import BaseModel
+from typing import Dict, List
+from datetime import datetime
+from accessDB import get_tournaments_in_daterange, get_matches_in_tournament
+
+class Player(BaseModel):
+    id: int
+    name: str
+    elo: float = 1500.0
+    elo_history: Dict[datetime, float] = {}
+
+    def update_elo(self, new_elo: float, date: datetime):
+        self.elo = new_elo
+        self.elo_history[date] = new_elo
+
+class Match(BaseModel):
+    winner: Player
+    loser: Player
+    date: datetime
+
+class EloModel:
+    def __init__(self, k_factor: float = 32):
+        self.players: Dict[int, Player] = {}
+        self.k_factor = k_factor
+
+    def add_player(self, player_id: int, player_name: str):
+        if player_id not in self.players:
+            self.players[player_id] = Player(id=player_id, name=player_name)
+
+    def get_player(self, player_id: int) -> Player:
+        return self.players.get(player_id)
+
+    def update_elo(self, match: Match):
+        winner = match.winner
+        loser = match.loser
+
+        updated_score_winner = 1 / (1 + 10 ** ((loser.elo - winner.elo) / 400))
+        updated_score_loser = 1 / (1 + 10 ** ((winner.elo - loser.elo) / 400))
+
+        winner.elo += self.k_factor * (1 - updated_score_winner)
+        loser.elo += self.k_factor * (0 - updated_score_loser)
+
+    def process_matches(self, matches: List[Match]):
+        for match in matches:
+            self.update_elo(match)
+
+def main(start_date: str, end_date: str):
+    elo = EloModel()
+    tournaments = get_tournaments_in_daterange(
+        datetime.strptime(start_date, '%Y-%m-%d'),
+        datetime.strptime(end_date, '%Y-%m-%d')
+    )
+
+    matches = get_matches_in_tournament(list(set(tournaments['Tour_ID'])))
+    for _, match in matches.iterrows():
+        winner_id = match['ID Winner_G']
+        loser_id = match['ID Loser_G']
+        winner_name = match['winnerName']
+        loser_name = match['loserName']
+
+        elo.add_player(winner_id, winner_name)
+        elo.add_player(loser_id, loser_name)
+    
+        match = Match(winner=winner_id, loser=loser_id, date=match['DATE_G'])
+        elo.update_elo(match)
+
+    for player in elo.players.values():
+        print(f"Player: {player.name}, ELO: {player.elo}")
+
+if __name__ == "__main__":
+    start_date = '2023-01-01'
+    end_date = '2023-12-31'
+    main(start_date, end_date)
+
+
